73e9843dbd8b415ced6277ce6dba382a0996e2a6
Rodolfo Domingos <rodolfo.dmn@gmail.com>
2020-06-19 15:09:38 -0300

    lidando com erros e lendo files

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,23 +1,36 @@
-use structopt::StructOpt;
+//use structopt::StructOpt;
 
-#[derive(StructOpt)]
-struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
+//#[derive(StructOpt)]
+//struct Cli {
+    //pattern: String,
+    //#[structopt(parse(from_os_str))]
+    //path: std::path::PathBuf,
+//}
 
-fn main() {
-    let args = Cli::from_args();
-    println!("{}, {:?}", args.pattern, args.path);
+//fn main() {
+    //let args = Cli::from_args();
+    //println!("{}, {:?}", args.pattern, args.path);
 
-    let content = std::fs::read_to_string(&args.path)
-        .expect("nao achei o file");
+    //let content = std::fs::read_to_string(&args.path)
+        //.expect("nao achei o file");
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    //for line in content.lines() {
+        //if line.contains(&args.pattern) {
+            //println!("{}", line);
+        //}
+    //}
 
+    //show_my_file();
+//}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let result = std::fs::read_to_string("teste.txt"); 
+
+    let content = match result {
+        Ok(content) => { content },
+        Err(error) => { return Err(error.into()); }
+    };
+
+    println!("Ó o que tinha la: {}", content);
+    Ok(())
 }
