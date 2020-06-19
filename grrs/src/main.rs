b0176afbfa78fffc1258b0262f7217da67de63bc
Rodolfo <rodolfo@Rodolfos-MacBook-Pro.local>
2020-06-19 12:06:27 -0300

    exemplo grrs do site

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,23 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{}, {:?}", args.pattern, args.path);
+
+    let content = std::fs::read_to_string(&args.path)
+        .expect("nao achei o file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
 }
